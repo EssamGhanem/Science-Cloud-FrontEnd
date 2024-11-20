b2b67128a9ae486d2ab909eb904c3461e7fda54c
Essam <essam.ghanem723@gmail.com>
2024-11-20 23:08:15 +0200

    "solving sessionId params error"

--- src/app/dashboard/sessions/[sessionId]/page.tsx
+++ src/app/dashboard/sessions/[sessionId]/page.tsx
@@ -3,17 +3,19 @@ import Session from '@/components/dashboardComponents/courses/session/Session'
 import React from 'react'
 import { courses } from '@/data';
 
-export default function page(params:{sessionId:string}) {
-  
-    const sessionId = params.sessionId;
-  console.log(sessionId);
-  const session = courses[1].sessions[1]; 
-return (
-  <>
-    <div dir='rtl' className='p-container pt-[40px] '>
-      <Session session={session} />
+export default function page({ params }:
 
-    </div>
-  </>
-)
+  { params: { sessionId: string } }
+) {
+  const sessionId = params.sessionId;
+  console.log(sessionId);
+  const session = courses[1].sessions[1];
+  return (
+    <>
+      <div dir='rtl' className='p-container pt-[40px] '>
+        <Session session={session} />
+
+      </div>
+    </>
+  )
 }
